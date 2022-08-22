cb408d7cb008a26d5fade0108fcd886c1e335d84
Paweł Musiał <Pawel.Musial@microsoft.wsei.edu.pl>
2022-08-22 22:00:24 +0200

    Odnowa

--- Film.xaml.cs
+++ Film.xaml.cs
@@ -26,19 +26,6 @@ namespace Projekt2._0
         {
             InitializeComponent();
 
-            string connectionString = "SERVER=(localdb)\\Local;DATABASE=master;USER ID=PAWEŁ\\pawel;PASSWORD=";
-
-            MySqlConnection connection = new MySqlConnection(connectionString);
-
-            MySqlCommand command = new MySqlCommand("select * from Filmy", connection);
-            connection.Open();
-            DataTable dt = new DataTable();
-
-            dt.Load(command.ExecuteReader());
-            connection.Close();
-
-            TabelaFilm.DataContext = dt;
-
         }
 
         private void PowrótFilm_Click(object sender, RoutedEventArgs e)
@@ -52,12 +39,12 @@ namespace Projekt2._0
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
-            //Projekt2._0.masterFilmoteka masterFilmoteka = ((Projekt2._0.masterFilmoteka)(this.FindResource("masterFilmoteka")));
-            //// Załaduj dane do tabeli Filmy. Możesz modyfikować ten kod w razie potrzeby.
-            //Projekt2._0.masterFilmotekaTableAdapters.FilmyTableAdapter masterFilmotekaFilmyTableAdapter = new Projekt2._0.masterFilmotekaTableAdapters.FilmyTableAdapter();
-            //masterFilmotekaFilmyTableAdapter.Fill(masterFilmoteka.Filmy);
-            //System.Windows.Data.CollectionViewSource filmyViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("filmyViewSource")));
-            //filmyViewSource.View.MoveCurrentToFirst();
+            Projekt2._0.masterFilmoteka masterFilmoteka = ((Projekt2._0.masterFilmoteka)(this.FindResource("masterFilmoteka")));
+            // Załaduj dane do tabeli Filmy. Możesz modyfikować ten kod w razie potrzeby.
+            Projekt2._0.masterFilmotekaTableAdapters.FilmyTableAdapter masterFilmotekaFilmyTableAdapter = new Projekt2._0.masterFilmotekaTableAdapters.FilmyTableAdapter();
+            masterFilmotekaFilmyTableAdapter.Fill(masterFilmoteka.Filmy);
+            System.Windows.Data.CollectionViewSource filmyViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("filmyViewSource")));
+            filmyViewSource.View.MoveCurrentToFirst();
         }
 
         private void WstawianieFilm_Click(object sender, RoutedEventArgs e)
