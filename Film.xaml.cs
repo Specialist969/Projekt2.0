152d6ac62771d9ef9c8bd4dd63a63e1c1042ed38
Paweł Musiał <Pawel.Musial@microsoft.wsei.edu.pl>
2022-08-28 16:07:38 +0200

    zmiany w oknie film

--- Film.xaml.cs
+++ Film.xaml.cs
@@ -1,9 +1,5 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,52 +21,7 @@ namespace Projekt2._0
     {
         public Film()
         {
-            //SqlConnection sqlConnection = new SqlConnection("Server=.\\Local;Database=master;Integrated Security=true");
-            //sqlConnection.Open();
-            //SqlCommand cmd = new SqlCommand("SELECT Filmy",sqlConnection);
-            //SqlDataReader reader = cmd.ExecuteReader();
-            //while ( reader.Read() )
-            //{
-            //    Console.WriteLine("(1) , (0) " , reader.GetString(0), reader.GetString(1));
-            //}
-            //reader.Close();
-            //sqlConnection.Close();
-
-            //if (Debugger.IsAttached)
-            //{
-            //    Console.ReadLine();
-            //}
-        }
-
-        private void PowrótFilm_Click(object sender, RoutedEventArgs e)
-        {
-            MainWindow rej = new MainWindow();
-            rej.Show();
-
-            this.Close();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
-            Projekt2._0.masterFilmoteka masterFilmoteka = ((Projekt2._0.masterFilmoteka)(this.FindResource("masterFilmoteka")));
-            // Załaduj dane do tabeli Filmy. Możesz modyfikować ten kod w razie potrzeby.
-            Projekt2._0.masterFilmotekaTableAdapters.FilmyTableAdapter masterFilmotekaFilmyTableAdapter = new Projekt2._0.masterFilmotekaTableAdapters.FilmyTableAdapter();
-            masterFilmotekaFilmyTableAdapter.Fill(masterFilmoteka.Filmy);
-            System.Windows.Data.CollectionViewSource filmyViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("filmyViewSource")));
-            filmyViewSource.View.MoveCurrentToFirst();
-        }
-
-        private void WstawianieFilm_Click(object sender, RoutedEventArgs e)
-        {
-            DodawanieFilmu rej = new DodawanieFilmu();
-            rej.Show();
-
-            this.Close();
-        }
-        private void TabelaFilm_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
+            InitializeComponent();
         }
     }
 }
